ecea2188494c0cb8175c019852a7a89704bc69db
andresvelazquez <andresvelazquez.leganes@gmail.com>
2018-03-15 00:21:55 +0100

    base placeholder

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 function start(){
 	var container = document.getElementById("container");
     while (container.childNodes.length > 0){
-	if (container.getElementsByTagName("div")[0]==container.firstChild){
+	if (container.getElementsByTagName("section")[0]==container.firstChild){
 	    view[view.length] = container.removeChild(container.firstChild);
 	}
 	else container.removeChild(container.firstChild);
     }
 }
 
-function show(counter){
+function show(page){
 	var container = document.getElementById("container");
     if (container.childNodes.length==0){
-		container.appendChild(view[counter]);
+		container.appendChild(view[page]);
     }
 	else{
-		container.replaceChild(view[counter], container.childNodes[0]); 
+		container.replaceChild(view[page], container.childNodes[0]); 
 	}
 }
 
